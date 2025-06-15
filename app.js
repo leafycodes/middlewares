fcd0736cbd7ea77d250b74b9f9af183a6fe66f5b
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-15 22:48:05 +0530

    logger middleware demo

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date(Date.now()).toString();
+  console.log(req.method, req.hostname, req.path, req.requestTime);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("root");
+});
